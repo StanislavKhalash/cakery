d531f12f37e20d5b117549c2cc57a68568ee9e7d
Stanislav Khalash <stanislav.khalash@gmail.com>
2018-01-20 18:52:33 +0100

    Added a nuspec dependency finder addin

---
+++ Addins/Cake.NuGetDependencies/NuGetDependenciesAliases.cs
@@ -0,0 +1,16 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using Cake.Core.IO;
+
+namespace Cake.NuGetDependencies
+{
+    public static class NuGetDependenciesAliases
+    {
+        [CakeMethodAlias]
+        public static void NuGetDependencies(this ICakeContext context, DirectoryPath tagetDirectoryPath)
+        {
+            var runner = new NuGetDependenciesFinder(tagetDirectoryPath, context.Environment.WorkingDirectory, context.FileSystem, context.Log);
+            runner.Run();
+        }
+    }
+}
---
+++ Addins/Cake.NuGetDependencies/NuGetDependenciesFinder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+
+namespace Cake.NuGetDependencies
+{
+    public class NuGetDependenciesFinder
+    {
+        private readonly DirectoryPath _targetDirectoryPath;
+        private readonly DirectoryPath _workingDirectoryPath;
+        private readonly IFileSystem _fileSystem;
+        private readonly ICakeLog _log;
+
+        public NuGetDependenciesFinder(
+            DirectoryPath targetDirectoryPath, 
+            DirectoryPath workingDirectoryPath,
+            IFileSystem fileSystem, 
+            ICakeLog log)
+        {
+            _targetDirectoryPath = targetDirectoryPath;
+            _workingDirectoryPath = workingDirectoryPath;
+            _fileSystem = fileSystem;
+            _log = log;
+        }
+
+        public void Run()
+        {
+            var referencedPackages = GetReferencedPackages();
+
+            var localNugetCacheDirectory = _fileSystem
+                .GetDirectory(_workingDirectoryPath)
+                .GetDirectories("packages", SearchScope.Current)
+                .First();
+
+            foreach (var packageName in referencedPackages)
+            {
+                var searchFilter = $"{packageName}*";
+
+                var packageDirectory = localNugetCacheDirectory.GetDirectories(searchFilter, SearchScope.Current).Last();
+                var packageFile = packageDirectory.GetFiles(searchFilter, SearchScope.Current).Last();
+
+                DumpPackageInfo(packageFile);
+            }
+        }
+
+        private HashSet<string> GetReferencedPackages()
+        {
+            HashSet<string> packageNames = new HashSet<string>();
+
+            var rootDirectory = _fileSystem.GetDirectory(_targetDirectoryPath);
+
+            var packageReferenceFiles = rootDirectory
+                .GetFiles("packages.config", SearchScope.Recursive)
+                .Select(it => new NuGet.PackageReferenceFile(it.Path.FullPath));
+
+            foreach (var packageReference in 
+                packageReferenceFiles.SelectMany(it => it.GetPackageReferences()))
+            {
+                packageNames.Add(packageReference.Id);
+            }
+
+            return packageNames;
+        }
+
+        private void DumpPackageInfo(IFile packageFile)
+        {
+            var fileStream = packageFile.Open(FileMode.Open, FileAccess.Read);
+            var zipPackage = new NuGet.ZipPackage(fileStream);
+
+            _log.Information(zipPackage.Description);
+            _log.Information(zipPackage.LicenseUrl);
+        }
+    }
+}
